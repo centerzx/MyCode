8b89c453ae4c1c9b822a54c32e70bb9d7a72d35a
zhongxin <277986425@qq.com>
2016-08-26 11:24:25 +0800

    添加工具类

--- app/src/main/java/com/center/mycode/activity/MainActivity.java
+++ app/src/main/java/com/center/mycode/activity/MainActivity.java
@@ -11,18 +11,18 @@ import android.view.animation.ScaleAnimation;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 import com.center.mycode.MyApplication;
 import com.center.mycode.R;
 import com.center.mycode.bean.User;
 import com.center.mycode.common.Constants;
 import com.center.mycode.face.IUserBiz;
 import com.kyleduo.switchbutton.SwitchButton;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends BaseActivity
 {
@@ -83,7 +83,38 @@ public class MainActivity extends BaseActivity
             }
         });
         setAnimation();
+        
+        //沉浸式效果
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+//        {
+//            View decorView=getWindow().getDecorView();
+        //全屏、主体内容占用系统导航栏的空间、主体内容占用系统状态栏的空间
+//            int option=View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|View
+// .SYSTEM_UI_FLAG_LAYOUT_STABLE;
+//            decorView.setSystemUiVisibility(option);
+//            getWindow().setStatusBarColor(Color.TRANSPARENT);
+//            getWindow().setNavigationBarColor(Color.TRANSPARENT);//导航栏为透明
+//        }
+        //隐藏ActionBar
+//        ActionBar actionBar=getSupportActionBar();
+//        actionBar.hide();
     }
+    
+    //沉浸式模式
+//    @Override
+//    public void onWindowFocusChanged(boolean hasFocus) {
+//        super.onWindowFocusChanged(hasFocus);
+//        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+//            View decorView = getWindow().getDecorView();
+//            decorView.setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+//        }
+//    }
     
     private class PressToSpeakListen implements View.OnTouchListener
     {
